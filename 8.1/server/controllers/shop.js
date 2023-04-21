0dcee245503ef2ff3c225b2f0e0eb56517e5c77e
hunglqFX18738 <113684973+hunglqFX18738@users.noreply.github.com>
2023-04-21 22:06:54 +0700

    Update shop.js

--- 8.1/server/controllers/shop.js
+++ 8.1/server/controllers/shop.js
@@ -11,7 +11,7 @@ exports.getProducts = (req, res, next) => {
 exports.getProduct = (req, res, next) => {
   const prodId = req.params.productId;
   Product.findById(prodId, product => {
-    res.json({ product: product });
+    res.json(product);
   });
 };
 
